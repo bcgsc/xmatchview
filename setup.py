4358749ee522c002157fb9ce11d41927dd3f0323
Zhuyi Xue <alfred532008@gmail.com>
2018-12-14 09:32:04 -0800

    refactored setup.py, updating packages, package_dir and package_data

--- setup.py
+++ setup.py
@@ -3,19 +3,13 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+
+from setuptools import setup
+
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-
-requirements = [
-    'pil>=1.1.7',
-]
-
-setup_requirements = []
-
-test_requirements = []
 
 setup(
     author="René L. Warren",
@@ -28,20 +22,28 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
     description="Cleavage site prediction via de novo assembly",
+
+    packages=['xmatchview'],
+    package_dir={'xmatchview': '.'},
+    package_data={'xmatchview': ['./test/*']},
     scripts=[
         'xmatchview.py',
         'xmatchview-conifer.py'
     ],
-    install_requires=requirements,
+    install_requires=[
+        'pil>=1.1.7',
+    ],
+
     license="GNU General Public License v3",
     long_description=readme,
     include_package_data=True,
     keywords='Genome visualization DNA sequence alignments Smith Waterman Alignments cross_match xmatchview',
     name='xmatchview',
-    packages=find_packages(),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
+
+    # setup_requires=[],
+    # test_suite='test',
+    # tests_require=[],
+
     url='https://github.com/bcgsc/xmatchview',
     version='1.1.1',
     zip_safe=False,
